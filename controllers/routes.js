9374cec985cb6886747dd5809c8b28fd0bf9f77e
Kaleb M <kay@kalebs-air.attlocal.net>
2020-03-25 20:07:21 -0700

    added servers and handlepars main code

---
+++ controllers/routes.js
@@ -0,0 +1,13 @@
+// need an reference to express
+var express = require('express');
+
+// need an reference to express.router
+var router = express.Router();
+
+router.get('/', function(req,res){
+    res.render('index');
+})
+
+
+
+
